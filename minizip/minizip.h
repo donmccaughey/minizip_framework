033e94fe3a42ff675ee00d4e44d219f94b283266
Don McCaughey <don@donm.cc>
2024-12-27 13:58:33 -0600

    Initial Commit

---
+++ minizip/minizip.h
@@ -0,0 +1,18 @@
+//
+//  minizip.h
+//  minizip
+//
+//  Created by Don McCaughey on 12/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for minizip.
+FOUNDATION_EXPORT double minizipVersionNumber;
+
+//! Project version string for minizip.
+FOUNDATION_EXPORT const unsigned char minizipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <minizip/PublicHeader.h>
+
+
